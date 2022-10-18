ecbc08b87e62177af6aac8036a270ba8ee0c674b
drahmedgabr <dr.ahmedgabr@yahoo.com>
2022-10-18 04:41:08 +0200

    1step customview

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,7 +2,7 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import kotlin.properties.Delegates
@@ -10,8 +10,16 @@ import kotlin.properties.Delegates
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    private var widthSize = 0
-    private var heightSize = 0
+    private var widthSize = 0f
+    private var heightSize = 0f
+
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 50.0f
+        typeface = Typeface.create( "", Typeface.BOLD)
+    }
 
     private val valueAnimator = ValueAnimator()
 
@@ -27,6 +35,9 @@ class LoadingButton @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        if (canvas != null) {
+            drawButton(canvas)
+        }
 
     }
 
@@ -38,9 +49,27 @@ class LoadingButton @JvmOverloads constructor(
             heightMeasureSpec,
             0
         )
-        widthSize = w
-        heightSize = h
+        widthSize = w.toFloat()
+        heightSize = h.toFloat()
         setMeasuredDimension(w, h)
+    }
+
+    private fun drawButton(canvas: Canvas){
+        drawRect(canvas)
+        drawText(canvas, "Download")
+    }
+
+    private fun drawRect(canvas: Canvas){
+        paint.color = resources.getColor(R.color.button_bg, context.theme)
+        canvas.drawRect(
+            RectF(
+                0f, 0f, widthSize, heightSize
+        ), paint)
+    }
+
+    private fun drawText(canvas: Canvas, text: String){
+        paint.color = resources.getColor(R.color.button_txt_color, context.theme)
+        canvas.drawText(text, widthSize/2, heightSize/2 - ((paint.descent() + paint.ascent()) / 2) , paint)
     }
 
 }
