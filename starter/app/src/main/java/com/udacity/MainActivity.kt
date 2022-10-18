14313a32573d1103fd781c3e3dfacf056a0aba64
drahmedgabr <dr.ahmedgabr@yahoo.com>
2022-10-18 22:03:55 +0200

    progress

--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,9 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +21,8 @@ import kotlinx.android.synthetic.main.content_main.*
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
+    private var selectedRadioIndex = -1
+    private var radioButtonText = ""
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
@@ -30,9 +35,29 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        custom_button.isEnabled = false
+        Toast.makeText(this, getText(R.string.select_option), Toast.LENGTH_LONG).show()
+
         custom_button.setOnClickListener {
+
+            // Another Way to tell user to select file
+
+//            if (selectedRadioIndex < 0) {
+//                Toast.makeText(this, getText(R.string.select_option), Toast.LENGTH_LONG).show()
+//            } else {
+//                download()
+//            }
+
             download()
         }
+
+        radioGroup.setOnCheckedChangeListener { radioGroup, i ->
+            val selectedButton = findViewById<RadioButton>(i)
+            selectedRadioIndex = radioGroup.indexOfChild(selectedButton)
+            radioButtonText = selectedButton.text.toString()
+            custom_button.isEnabled = true
+        }
+
 
     }
 
@@ -44,7 +69,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun download() {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(URL_LIST[selectedRadioIndex]))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -57,6 +82,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
+        private val URL_LIST = listOf<String>("https://github.com/bumptech/glide","https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter","https://github.com/square/retrofit")
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
